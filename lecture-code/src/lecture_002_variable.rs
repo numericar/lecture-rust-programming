843d6570606395b1db7d5d3f8b5a62d5841a2f34
Alphanumeric Str <150432237+numericar@users.noreply.github.com>
2025-01-17 17:35:53 +0700

    lecture: intro

---
+++ lecture-code/src/lecture_002_variable.rs
@@ -0,0 +1,8 @@
+pub fn process() {
+    let x: i32 = 5;
+    let mut y: i32 = 10;
+    y = 20;
+
+    println!("x : {}", x);
+    println!("y : {}", y);
+}
--- lecture-code/src/main.rs
+++ lecture-code/src/main.rs
@@ -1,3 +1,8 @@
+mod lecture_002_variable;
+
 fn main() {
+    // ! (macro) เป็นสัญลักษณ์ที่บอกว่าจะทำงานตอน compile time/run time
     println!("Hello, world!");
+
+    lecture_002_variable::process();
 }
